d64ee6c382bfda13f3b80026515777c7eb62d2e7
MichaelStroganov <90139406+MichaelStroganov@users.noreply.github.com>
2023-04-08 09:32:05 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,4 +5,4 @@ import numpy as np
 chat_id = 123456 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    return x.mean() # Ваш ответ
+    return x.mean()/18 # Ваш ответ
